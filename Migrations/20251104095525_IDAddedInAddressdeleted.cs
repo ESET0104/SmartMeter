7d8b9007d2253a2b38e662e15fd06e1d9ba7c595
ESET0143 <melam.surendra@esyasoft.com>
2025-11-04 15:26:12 +0530

    Removed dummy column

---
+++ Migrations/20251104095525_IDAddedInAddressdeleted.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartMeterWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class IDAddedInAddressdeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Addresses",
+                newName: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Addresses",
+                newName: "Id");
+        }
+    }
+}
